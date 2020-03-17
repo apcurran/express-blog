fcf03690fbb476ed8d465c884193c8406dcf38a7
apc <alexcurran.va@gmail.com>
2020-03-17 15:37:34 -0500

    Added show-post view

--- routes/articles.js
+++ routes/articles.js
@@ -2,12 +2,16 @@ const express = require("express");
 const router = express.Router();
 const Article = require("../models/Article");
 
-// router.get("/:id", (req, res) => {
+router.get("/post/:id", async (req, res) => {
+    const article = await Article.findById(req.params.id);
 
-// });
+    if (article == null) res.redirect("/");
+
+    res.render("articles/show-post", { title: article.title, article: article });
+});
 
 router.get("/new", (req, res) => {
-    res.render("articles/new", { title: "New Post" });
+    res.render("articles/new", { title: "New Post", error: "", article: new Article() });
 });
 
 router.post("/new", async (req, res, next) => {
@@ -16,12 +20,11 @@ router.post("/new", async (req, res, next) => {
         description: req.body.description,
         markdown: req.body.markdown
     });
-    
-    try {
 
+    try {
         article = await article.save();
 
-        res.redirect(`/article/${article._id}`)
+        res.redirect(`/articles/post/${article._id}`)
     } catch (err) {
         res.render("articles/new", { title: "New Post", error: err, article: article });
 
