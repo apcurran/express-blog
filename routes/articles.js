96672e35d3d23266e972eb2a0d220a6e180ff832
apc <alexcurran.va@gmail.com>
2020-03-17 12:28:38 -0500

    Setup db

--- routes/articles.js
+++ routes/articles.js
@@ -5,4 +5,8 @@ router.get("/new", (req, res) => {
     res.render("articles/new", { title: "New Post" });
 });
 
+router.post("/new", (req, res) => {
+    
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 const expressLayouts = require("express-ejs-layouts");
 require("dotenv").config();
@@ -9,6 +10,11 @@ require("dotenv").config();
 if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
+
+// DB Setup
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
 
 app.set("view engine", "ejs");
 app.set("layout", "layouts/layout");
